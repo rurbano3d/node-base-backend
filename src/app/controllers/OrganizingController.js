4f60211ba1357284d775423a2a33ff2ac694e0a4
rurbano3d <rurbano3d@gmail.com>
2019-10-23 15:15:29 -0300

    chore:include path file

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -1,8 +1,17 @@
 import Meetup from '../models/Meetup';
+import File from '../models/File';
 
 class OrganizingController {
   async index(req, res) {
-    const meetups = await Meetup.findAll({ where: { user_id: req.userId } });
+    const meetups = await Meetup.findAll({
+      where: { user_id: req.userId },
+      include: [
+        {
+          model: File,
+          attributes: ['path'],
+        },
+      ],
+    });
 
     return res.json(meetups);
   }
